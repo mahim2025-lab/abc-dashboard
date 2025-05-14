d47f92d313286b4f3d1fc5f16296d196ac907618
mahim2025-lab <23039142003@slms.ssodl.edu.in>
2025-05-14 19:43:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,11 +150,12 @@ def show_chatbot():
         user_query = st.text_input("Type your question here:")
         if user_query:
             with st.spinner('Thinking...'):
-                model = genai.GenerativeModel("gemini-1.5-pro")
-                response = model.generate_content(
-                    f"You are a KPI dashboard assistant. Answer based on KPIs.\nQuestion: {user_query}"
-                )
-                st.success(response.text)
+               try:
+                    model = genai.GenerativeModel("gemini-pro")
+                    response = model.generate_content(f"Question: {user_query}")
+                    st.success(response.text)
+                except Exception as e:
+                    st.error("Chatbot is currently unavailable or quota is exhausted. Please try again later."
 
 def main():
     kpi_df = load_kpi_data()
